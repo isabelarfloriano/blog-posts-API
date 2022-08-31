099a3b61c89d5e47beb6791621a546d3ed89d8e1
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-31 19:19:30 -0300

    feat: create the migration for BlogPosts

---
+++ src/database/migrations/20220831214709-create-blog-post.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        foreignKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      update: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
