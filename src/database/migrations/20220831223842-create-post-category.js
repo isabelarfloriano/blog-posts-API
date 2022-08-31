4881467a2a9cfab501a268ce69eb3d57e9c6d347
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-31 19:45:18 -0300

    feat: create the migration for PostCategories

---
+++ src/database/migrations/20220831223842-create-post-category.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        foreignKey: true,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        foreignKey: true,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
