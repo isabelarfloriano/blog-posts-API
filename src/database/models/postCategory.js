1cd5f66f6a6999db9d0c7dac82bdaf50172380d4
Isabela Rozani Floriano <irf161295@gmail.com>
2022-09-01 14:29:18 -0300

    feat: create the model for PostCategories

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'PostCategories'
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      as: 'blogposts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+
+  return PostCategory;
+};
