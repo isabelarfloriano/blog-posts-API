cd51eb6080091d0bbf466135347da94fef2969a8
Isabela Rozani Floriano <irf161295@gmail.com>
2022-09-02 11:26:23 -0300

    fix: revert server modifications

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const app = require('./api');
-const routes = require('./database/routes');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -8,13 +7,6 @@ const port = process.env.API_PORT || 3000;
 // não remova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
-});
-
-app.use('/login', routes.loginRouter);
-app.use('/user', routes.userRouter);
-app.use((err, req, res, next) => {
-  const [code, message] = err.message.split('|');
-  return res.status(code).json({ message });
 });
 
 app.listen(port, () => console.log('ouvindo porta', port));
